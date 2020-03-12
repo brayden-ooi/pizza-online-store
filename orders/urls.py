0d770395e31fb8b64c8cc466d870e96464930a92
Brayden Ooi <brayden.ooi@hotmail.com>
2020-03-13 02:38:58 +0800

    app menu troubleshoot

--- orders/urls.py
+++ orders/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path("api/pending_orders", views.pending_orders, name="pending_orders"),
     path("api/payment", views.payment, name="payment"),
     path("api/order", views.order, name="order"),
-    path('/', views.index, name="index"),
+    path('', views.index, name="index"),
 ]
