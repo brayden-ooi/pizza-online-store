aa54aeca258e680e38d2385aee88e2a7a3a95e44
Brayden Ooi <brayden.ooi@hotmail.com>
2020-03-13 17:55:04 +0800

    app api request troubleshooting

--- orders/urls.py
+++ orders/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path
-from django.conf.urls import include, url
+from django.urls import path, re_path
+from django.conf.urls import include
 
 from . import views
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path("api/pending_orders", views.pending_orders, name="pending_orders"),
     path("api/payment", views.payment, name="payment"),
     path("api/order", views.order, name="order"),
-    path('', views.index, name="index"),
+    path("", views.index, name="index"),
+    re_path(r"^(?!api/).*", views.index, name="fallback"),
 ]
